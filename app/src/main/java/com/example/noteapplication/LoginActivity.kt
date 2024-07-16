add193cde7ae2cde5f09472b2eeddc9286b9a7f3
MoeinPQ <moien.barsa@gmail.com>
2024-07-16 20:08:01 +0330

    use intent for switch from login activity to main activity & active view binding

--- app/src/main/java/com/example/noteapplication/LoginActivity.kt
+++ app/src/main/java/com/example/noteapplication/LoginActivity.kt
@@ -1,11 +1,30 @@
 package com.example.noteapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.noteapplication.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.enterButton.setOnClickListener {
+
+            val userEditText = binding.userNameEditText.text.toString()
+            val passwordEditText = binding.passEditText.text.toString()
+
+            if(userEditText.isEmpty() || passwordEditText.isEmpty()) {
+                Toast.makeText(this, "لطفا تمامی مقادیر را پر کنید", Toast.LENGTH_SHORT).show()
+            }else{
+                val intent = Intent(this, MainActivity::class.java)
+                intent.putExtra("userName", userEditText)
+                startActivity(intent)
+            }
+        }
     }
 }
--- app/src/main/java/com/example/noteapplication/MainActivity.kt
+++ app/src/main/java/com/example/noteapplication/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.noteapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val intent = intent
+        val name = intent.getStringExtra("userName")
+        Toast.makeText(this, name, Toast.LENGTH_SHORT).show()
     }
 }
