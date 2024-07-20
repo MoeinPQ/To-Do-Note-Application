77cb65b9bde16d8ae629de97262759ac043c683b
MoeinPQ <moien.barsa@gmail.com>
2024-07-21 00:26:42 +0330

    write queries

--- app/src/main/java/com/example/noteapplication/database/NotesDao.kt
+++ app/src/main/java/com/example/noteapplication/database/NotesDao.kt
@@ -1,4 +1,17 @@
 package com.example.noteapplication.database
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+
+
+@Dao
 interface NotesDao {
+
+    @Query("SELECT * FROM notes")
+    fun getAllNotes() : MutableList<NotesData>
+
+    @Insert
+    fun insertNotes(notesData: NotesData)
+
 }
