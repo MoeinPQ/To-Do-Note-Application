4492c55b2c58d5a94d5a542f75672c57dd4273af
MoeinPQ <moien.barsa@gmail.com>
2024-07-21 14:50:30 +0330

    complete data base builder class

--- app/src/main/java/com/example/noteapplication/database/NotesDatabase.kt
+++ app/src/main/java/com/example/noteapplication/database/NotesDatabase.kt
@@ -1,4 +1,18 @@
 package com.example.noteapplication.database
 
-abstract class NotesDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+
+@Database(entities = [NotesData::class] , version = 1)
+abstract class NotesDatabase() : RoomDatabase() {
+    abstract fun getNotesDao() : NotesDao
+
+    fun buildDatabase(context : Context){
+        Room.databaseBuilder(context.applicationContext,NotesDatabase::class.java,"notes.db")
+            .allowMainThreadQueries()
+            .build()
+    }
 }
